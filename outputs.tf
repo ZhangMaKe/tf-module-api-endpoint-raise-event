1bc0e38f67bb5e94efb04152d62224a0ffaaec0f
zmk <zhangmake86@gmail.com>
2025-11-06 17:27:52 +0800

    Added outputs for role arn, policy arns, integration id, and route id

---
+++ outputs.tf
@@ -0,0 +1,19 @@
+output "role_arn" {
+  description = "The ARN of the IAM role created for API Gateway to assume."
+  value       = module.role_and_policy.role_arn
+}
+
+output "policy_arns" {
+    description = "A list of the IDs of the created IAM policies."
+    value = module.role_and_policy.policy_arns
+}
+
+output "integration_id" {
+    description = "The value of the created API Gateway V2 integration."
+    value = aws_apigatewayv2_integration.eventbridge_integration.id
+}
+
+output "route_id" {
+    description = "The ID of the created API Gateway V2 route."
+    value = aws_apigatewayv2_route.apigw_route.id
+}
